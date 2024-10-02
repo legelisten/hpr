a9ce555221d4716565f73ff42d3bbcda6ed85394
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2024-10-02 12:16:39 +0200

    Update code to handle new (but invalid) HTML
    The code currently being served at register.helsedirektoratet.no/Hpr is
    invalid. It is self-closing the opening <html> tag.
    
    This adds some bug fixing code for that invalid HTML, allowing Mechanize
    to properly parse it.
    
    We also add the `handler` form attribute, without which the search
    fails.

--- lib/hpr/fetcher.rb
+++ lib/hpr/fetcher.rb
@@ -20,16 +20,16 @@ module Hpr
 
     def fetch
       mechanize.get(BASE_URL) do |page|
-        @page = page.form_with(action: FORM_ACTION) do |f|
-          f.Number = hpr_number
-        end.submit
+        page.body = html_bugfix(page.body) # This can hopefully be removed in the future
+        @page = submit_search_form(page)
+        @page.body = html_bugfix(@page.body)
 
         if hpr_number_not_found?
           raise Hpr::InvalidHprNumberError, "HPR number: #{hpr_number}"
         end
       end
 
-      @page.body
+      html_bugfix(@page.body)
     end
 
   private
@@ -37,13 +37,39 @@ module Hpr
     attr_reader :page, :hpr_number
 
     def hpr_number_not_found?
-      !page.search("//text()[contains(.,'HPR-nummer ikke funnet') or contains(.,'Vennligst oppgi HPR/ID-nummer')]").empty?
+      # All permutations of "not found" we've encountered. Some of these
+      # may be outdated. The latest found is added last.
+      !page.search("//text()[contains(.,'HPR-nummer ikke funnet') \
+        or contains(.,'Vennligst oppgi HPR/ID-nummer') \
+        or contains(.,'Fant ingen personer')]").empty?
     end
 
     def mechanize
       ::Mechanize.new do |agent|
         agent.user_agent_alias = AGENT_ALIASES.sample
       end
+    end
+
+    def submit_search_form(page)
+      form = page.forms_with(action: FORM_ACTION)[0]
+      form['hprNumber'] = hpr_number
+      form['handler'] = 'HprNumberSubmit'
+      form.submit
+    end
+
+    ##
+    # Fixes a self closing opening <html> tag that Mechanize can't handle
+    #
+    # The current version of the HPR website has a self closing <html> tag
+    # that Mechanize can't handle. This method fixes that by removing the
+    # self closing slash and parsing the HTML using Nokogiri, which is better
+    # at handling broken HTML. This correctly adds the closing </html> tag.
+    #
+    # Hopefully this can be removed by the time you read this.
+    #
+    def html_bugfix(html)
+      html.gsub!('<html lang="en" />', '<html lang="en">')
+      Nokogiri::HTML(html).to_html
     end
   end
 end
